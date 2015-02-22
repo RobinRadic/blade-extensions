fa6826e54cc610dd13736e30c5307b9f7f678229
robinradic <robin@radic.nl>
2015-02-22 04:14:13 +0100

    Altered BladeExtenderTrait so it can be used outside of the current package scope by allowing manual $class->directives and $class->blacklist setting.

--- src/Traits/BladeExtenderTrait.php
+++ src/Traits/BladeExtenderTrait.php
@@ -5,12 +5,12 @@ use Illuminate\Foundation\Application;
 /**
  * Adds an static function `attach` that if called, will instanciate and execute all functions as blade extends
  *
- * @package        Radic\BladeExtensions
- * @version        2.1.0
- * @author         Robin Radic
- * @license        MIT License - http://radic.mit-license.org
+ * @package            Radic\BladeExtensions
+ * @version            2.1.0
+ * @author             Robin Radic
+ * @license            MIT License - http://radic.mit-license.org
  * @copyright      (c) 2011-2015, Robin Radic
- * @link           http://robin.radic.nl/blade-extensions
+ * @link               http://robin.radic.nl/blade-extensions
  *
  */
 trait BladeExtenderTrait
@@ -21,56 +21,39 @@ trait BladeExtenderTrait
      *
      * @var array
      */
-    public $blacklist = [];
+    public $blacklist;
+
+    public $directives;
 
     /**
      * Instanciate and execute all functions as blade extends
      *
-     * @param Application $app
+     * @param Application $app        The current application
      */
     public static function attach(Application $app)
     {
+        /** @var \Illuminate\View\Compilers\BladeCompiler $blade */
         $blade = $app['view']->getEngineResolver()->resolve('blade')->getCompiler();
-        $directives = $app['config']->get('blade-extensions.directives');
-        $class = new static;
-        foreach (get_class_methods($class) as $method) {
-            if ($method == 'attach') {
-                continue;
-            }
-            if (is_array($class->blacklist) && in_array($method, $class->blacklist)) {
+
+        $class      = new static;
+        $directives = isset($class->directives) ? $class->directives : $app['config']->get('blade-extensions.directives');
+        $blacklist  = isset($class->blacklist) ? $class->blacklist : $app['config']->get('blade-extensions.blacklist');
+
+        foreach (get_class_methods($class) as $method)
+        {
+            if ($method == 'attach' or (is_array($blacklist) && in_array($method, $blacklist)))
+            {
                 continue;
             }
 
             $directive = isset($directives[$method]) ? $directives[$method] : false;
 
             $blade->extend(
-                function ($value) use ($app, $class, $blade, $method, $directive) {
+                function ($value) use ($app, $class, $blade, $method, $directive)
+                {
                     return $class->$method($value, $directive, $app, $blade);
                 }
             );
         }
     }
-
-    /**
-     * Get the blacklist array
-     * @return array
-     */
-    public function getBlacklist()
-    {
-        return $this->blacklist;
-    }
-
-    /**
-     * Set the blacklist array
-     * @param array $blacklist
-     * @return $this
-     */
-    public function setBlacklist(array $blacklist)
-    {
-        $this->blacklist = $blacklist;
-
-        return $this;
-    }
-
-
 }
