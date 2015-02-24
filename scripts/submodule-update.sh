0880d7008d2fddeef3599dc58034ac24533d6762
robinradic <robin@radic.nl>
2015-02-24 23:58:33 +0100

    updated build

--- scripts/submodule-update.sh
+++ scripts/submodule-update.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # http://stackoverflow.com/questions/1777854/git-submodules-specify-a-branch-tag/18799234#18799234
-git submodule update --init --recursive --remote --no-fetch
+git submodule update --init --recursive --remote --force
+
 
 # get out of detached head state
-git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch; git pull'
+git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git fetch --all; git checkout $branch; git pull'
+
