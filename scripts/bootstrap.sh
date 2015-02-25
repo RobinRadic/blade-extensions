b246d1e563e6421acdf38d391ee4145ba6801d83
robinradic <robin@radic.nl>
2015-02-25 06:01:24 +0100

    test

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # http://stackoverflow.com/questions/1777854/git-submodules-specify-a-branch-tag/18799234#18799234
-git submodule update --init --recursive --remote --no-fetch
+git submodule update --init --recursive --remote --force
 
 # get out of detached head state
-git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch; git pull'
+git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git fetch --all; git checkout $branch; git pull'
 
 cp -f scripts/pre-commit .git/hooks/pre-commit
 echo "Added git pre-commit hook"
