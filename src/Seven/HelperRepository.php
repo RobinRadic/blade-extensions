d44d5ec541e59aea3dcdee8b9c630398ed7641b2
Robin Radic <robin@radic.nl>
2016-08-16 21:50:30 +0200

    added helpers

---
+++ src/Seven/HelperRepository.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Part of the Caffeinated PHP packages.
+ *
+ * MIT License and copyright information bundled with this package in the LICENSE file
+ */
+namespace Radic\BladeExtensions\Seven;
+
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\Traits\Macroable;
+
+/**
+ * This is the HelperRepository.
+ *
+ * @package        Radic\BladeExtensions
+ * @author         Caffeinated Dev Team
+ * @copyright      Copyright (c) 2015, Caffeinated
+ * @license        https://tldrlegal.com/license/mit-license MIT License
+ */
+class HelperRepository
+{
+    use Macroable;
+
+    protected $helpers;
+
+    protected $container;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+        $this->helpers   = [ ];
+    }
+
+    public function put($key, $data)
+    {
+
+        $this->helpers[ $key ] = $data;
+    }
+
+    public function has($key)
+    {
+        return isset($this->helpers[ $key ]);
+    }
+
+    public function get($key, $default = null)
+    {
+        return $this->has($key) ? $this->helpers[ $key ] : $default;
+    }
+}
---
+++ src/Seven/Helpers/Loop/Loop.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * Represents the $loop variable in the foreach directive. Handles all data.
+ */
+namespace Radic\BladeExtensions\Helpers;
+
+/**
+ * Represents the $loop variable in the foreach directive. Handles all data.
+ *
+ * @package        Radic\BladeExtensions
+ * @subpackage     Helpers
+ * @version        2.1.0
+ * @author         Robin Radic
+ * @license        MIT License - http://radic.mit-license.org
+ * @copyright      2011-2015, Robin Radic - Radic Technologies
+ * @link           http://robin.radic.nl/blade-extensions
+ *
+ *
+ * @property int  $index1
+ * @property int  $index
+ * @property int  $revindex1
+ * @property int  $revindex
+ * @property bool $first
+ * @property bool $last
+ * @property bool $odd
+ * @property bool $even
+ * @property int  $total
+ * @property Loop $parentLoop
+ *
+ *
+ */
+class Loop
+{
+
+    /**
+     * The array that is being iterated
+     *
+     * @var array
+     */
+    protected $items = [ ];
+
+    /**
+     * The data for the current $loop item
+     *
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * The parent loop, if any
+     *
+     * @var Loop
+     */
+    protected $parentLoop;
+
+    protected $loopFactory;
+
+    /**
+     * Sets the parent loop
+     *
+     * @param Loop $parentLoop
+     * {@inheritdocs}
+     */
+    public function setParentLoop(Loop $parentLoop)
+    {
+        $this->parentLoop       = $parentLoop;
+        $this->data[ 'parent' ] = $parentLoop;
+    }
+
+    /**
+     * Returns the full loop stack of the LoopFactory
+     *
+     * @return array
+     */
+    public function getLoopStack()
+    {
+        return $this->loopFactory->getStack();
+    }
+
+    /**
+     * Resets the loop stack of the LoopFactory
+     */
+    public function resetLoopStack()
+    {
+        $this->loopFactory->reset();
+    }
+
+    /**
+     * Instantiates the class
+     *
+     * @param array $items The array that's being iterated
+     */
+    public function __construct(LoopFactory $loopFactory, $items)
+    {
+        $this->loopFactory = $loopFactory;
+        $this->setItems($items);
+    }
+
+    /**
+     * Sets the array to monitor
+     *
+     * @param array $items The array that's being iterated
+     */
+    public function setItems($items)
+    {
+        if ( isset($data) ) {
+            return;
+        }
+        $this->items = $items;
+        $total       = count($items);
+        $this->data  = [
+            'index1'    => 1,
+            'index'     => 0,
+            'revindex1' => $total,
+            'revindex'  => $total - 1,
+            'first'     => true,
+            'last'      => false,
+            'odd'       => false,
+            'even'      => true,
+            'length'    => $total,
+        ];
+    }
+
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * Magic method to access the loop data properties
+     *
+     * @param $key
+     *
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->data[ $key ];
+    }
+
+    /**
+     * To be called first in a loop before anything else
+     */
+    public function before()
+    {
+        if ( $this->data[ 'index' ] % 2 == 0 ) {
+            $this->data[ 'odd' ]  = false;
+            $this->data[ 'even' ] = true;
+        } else {
+            $this->data[ 'odd' ]  = true;
+            $this->data[ 'even' ] = false;
+        }
+        if ( $this->data[ 'index' ] == 0 ) {
+            $this->data[ 'first' ] = true;
+        } else {
+            $this->data[ 'first' ] = false;
+        }
+        if ( $this->data[ 'revindex' ] == 0 ) {
+            $this->data[ 'last' ] = true;
+        } else {
+            $this->data[ 'last' ] = false;
+        }
+    }
+
+    /**
+     * To be called last in a loop after everything else
+     */
+    public function after()
+    {
+        $this->data[ 'index' ]++;
+        $this->data[ 'index1' ]++;
+        $this->data[ 'revindex' ]--;
+        $this->data[ 'revindex1' ]--;
+    }
+}
