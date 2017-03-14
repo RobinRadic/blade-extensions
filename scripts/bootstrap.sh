3b760d07b4899ef960034c86dc7044191f940095
Robin Radic <robin@radic.nl>
2017-03-14 19:55:21 +0100

    test for travis

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,8 +3,8 @@ mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -rf "$mydir/../build" >> /dev/null
 
-chmod +x "$mydir/pre-commit"
-cp "$mydir/pre-commit" "$mydir/../.git/hooks/pre-commit"
+#chmod +x "$mydir/pre-commit"
+#cp "$mydir/pre-commit" "$mydir/../.git/hooks/pre-commit"
 
 cd "$mydir/.."
 git clone https://github.com/laradic/php-build-tools build
