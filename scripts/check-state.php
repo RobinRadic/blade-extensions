49160bd7acdabe3f66336c85318287b54f9ea09c
Robin Radic <robin@radic.nl>
2017-03-19 21:11:35 +0100

    test for travis

--- scripts/check-state.php
+++ scripts/check-state.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 /**
  * Copyright (c) 2017. Robin Radic.
@@ -5,26 +6,19 @@
  * The license can be found in the package and online at https://radic.mit-license.org.
  *
  * @copyright 2017 Robin Radic
- * @license https://radic.mit-license.org MIT License
- * @version 7.0.0 Radic\BladeExtensions
+ * @license   https://radic.mit-license.org MIT License
+ * @version   7.0.0 Radic\BladeExtensions
  */
-
-
-function run()
-{
-    $content = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . '.curl-output');
-    if ($content === false) {
-        return 3;
-    }
-    $content = json_decode($content, true);
-    $state   = $content[ 'build' ][ 'state' ];
-    if ($state === 'failed' || $state === 'canceled') {
-        return 1;
-    } elseif ($state === 'passed') {
-        return 0;
-    } else {
-        return 2;
-    }
+$content = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . '.curl-output');
+if ($content === false) {
+    echo '3';
 }
-
-echo run();
+$content = json_decode($content, true);
+$state   = $content[ 'build' ][ 'state' ];
+if ($state === 'failed' || $state === 'canceled') {
+    echo '1';
+} elseif ($state === 'passed') {
+    echo '0';
+} else {
+    echo '2';
+}
