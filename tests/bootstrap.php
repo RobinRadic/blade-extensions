892db65e87eb7d1c8b0d050c5ecc73af5a542ffd
Robin Radic <robin@radic.nl>
2018-08-16 02:36:39 +0200

    add test bootstrap file with class alias

---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
